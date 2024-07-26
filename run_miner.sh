ff80a0b1d53bdc302681ac21491c9b7edaebe755
hadiportofolio <117813252+hadiportofolio@users.noreply.github.com>
2024-07-26 09:45:19 +0700

    Create run_miner.sh

---
+++ run_miner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cp>
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RSvLhDp2ZoRcovoaxgfrG1JggNFuiRZAf3.PC2 -p x --cpu 36
+
+
+
+
